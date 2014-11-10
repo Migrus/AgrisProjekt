9d7151b3356099ef279ad67a803175ef6db71902
migrus <michalgrus@seznam.cz>
2014-11-10 12:03:34 +0100

    1

--- app/src/main/java/ttrmg/pef/agrisprojekt/TextFragment.java
+++ app/src/main/java/ttrmg/pef/agrisprojekt/TextFragment.java
@@ -7,22 +7,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 import android.widget.TextView;
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 
 
 
 public class TextFragment extends Fragment {
-    TextView text,vers;
+    TextView text,ceny;
+    TableLayout tab;
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.text_fragment, container, false);
-        text= (TextView) view.findViewById(R.id.AndroidOs);
-        vers= (TextView)view.findViewById(R.id.Version);
+        text= (TextView) view.findViewById(R.id.Burzy);
+        ceny= (TextView)view.findViewById(R.id.Ceny);
         return view;
     }
     public void change(String txt, String txt1){
         text.setText(txt);
-        vers.setText(txt1);
+        ceny.setText(txt1);
     }
 }
